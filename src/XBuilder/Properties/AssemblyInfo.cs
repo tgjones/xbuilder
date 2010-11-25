bf4b15a010667d5ebd5daec0f9f6505702f521cf
Tim Jones <github@roastedamoeba.com>
2010-11-26 02:56:23 +0800

    Updated version number to 0.1.0.0

--- src/XBuilder/Properties/AssemblyInfo.cs
+++ src/XBuilder/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ModelViewer")]
+[assembly: AssemblyTitle("XBuilder")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Formosatek Ltd")]
-[assembly: AssemblyProduct("ModelViewer")]
+[assembly: AssemblyProduct("XBuilder")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]   
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
 
